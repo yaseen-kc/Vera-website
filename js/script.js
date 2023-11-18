fe323dc479ac19dd2232f738ecde9a439ef4584c
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2023-11-18 21:46:03 +0530

    Nav Scroll

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 // Replace Texy In Header
-const checkReplace = document.querySelector(".replace-me");
+const checkReplace = document.querySelector('.replace-me');
 
 if (checkReplace !== null) {
-const replace = new ReplaceMe(checkReplace,{
+  const replace = new ReplaceMe(checkReplace, {
     animation: 'animated fadeIn',
-    speed:500,
-    separator:',',
+    speed: 500,
+    separator: ',',
     loopCount: 'infinite',
     autoRun: true,
-})
+  });
 }
+
+//User Scroll For Navbar
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('border-bottom');
+      navbar.classList.remove('border-secondary');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded',userScroll)
